a34ac3fbc8462fac700fa976bd343ccad8ea9956
Sebastian Hurtado <sebastian.hurtado@utec.edu.pe>
2018-11-08 00:04:09 -0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-#include <GL/glut.h>
+
 #include <iostream>
 
 #include "matrix.h"
@@ -6,7 +6,34 @@
 using namespace std;
 
 int main(int argc, char *argv[]) {
-    Matrix<int> a(5, 5);
+    Matrix<int> a(4,4);
+    Matrix<int> b(4,4);
+    Matrix<int> c(4,4);
+
+    for(int i=0; i<4; i++){
+        for (int j=0; j<4; j++){
+            a.set(i, j, i+j);
+        }
+    }
+    for(int i=0; i<4; i++){
+        for (int j=0; j<4; j++){
+            b.set(i, j, 1);
+        }
+    }
+    Matrix<int> d(2,2);
+    d.set(0,1,2);
+    d.printMatrix();
+    cout << endl;
+    (d.transposed()).printMatrix();
+    cout << endl;
+    d= d.transposed();
+    d.printMatrix();
+
+
+
+
+
+
 
     system("PAUSE");
     return EXIT_SUCCESS;
