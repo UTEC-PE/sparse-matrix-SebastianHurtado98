48632738adf94315cff32e4a7e314c40dac62b58
Sebastian Hurtado <sebastian.hurtado@utec.edu.pe>
2018-11-12 09:49:53 -0500

    Random test

--- main.cpp
+++ main.cpp
@@ -1,16 +1,22 @@
 
 #include <iostream>
-
 #include "matrix.h"
 
 using namespace std;
 
 int main(int argc, char *argv[]) {
 
+    Matrix<int> r(5,5);
     Matrix<int> a(4,4);
     Matrix<int> b(4,4);
     Matrix<int> c(4,4);
 
+    for (int i=0; i<5; i++){
+        for (int j=0; j<5; j++)
+        {
+            r.set(i,j,rand()%10);
+        }
+    }
     for(int i=0; i<4; i++){
         for (int j=0; j<4; j++){
             a.set(i, j, i+j);
@@ -31,6 +37,7 @@ int main(int argc, char *argv[]) {
     d= d.transposed();
     d.printMatrix();
      */
+    r.printMatrix();
 
     a.set(4, 4, 5);
 
